24b8f2ded8253265ae597a0c070c22dd6188cfc7
Ludusamo <ludusamo@gmail.com>
2014-04-27 16:54:49 -0400

    Completed

--- js/game.js
+++ js/game.js
@@ -1,54 +1,18 @@
 var WIDTH = 800, HEIGHT = 600;
 var game = createBasicGame(WIDTH, HEIGHT, Phaser.CANVAS);
 
-var slimy;
-var music;
+addState('mainMenu', MainMenu, true);
+addState('town', Town);
+addState('arena', Arena);
 
 function preload() {
-	game.load.spritesheet("slimy", "assets/stretched.png", 32, 32);
-	loadAudio("music", "assets/music/test.ogg");
+
 }
 
 function create() {
-	// Music
-	// addMusic("music", music);
-	// playMusic(music);
 
-	// Adding Slimy to the world
-	slimy = game.add.sprite(0, 0, "slimy");
-
-	// Enabling Arcade Physics
-	enableArcadePhysics(slimy);
-
-	slimy.body.bounce.y = 0.2;
-	slimy.body.gravity.y = 300;
-	slimy.body.collideWorldBounds = true;
-
-	enableInput(slimy);
-	slimy.input.enableDrag(true);
-
-	// Slimy Animation
-	addAnimation(slimy, "blink", [0, 1], 2, true);
-	animationStart(slimy, "blink");
-
-	saveObject("slimy", slimy);
 }
 
 function update() {
-	slimy.body.velocity.x = 0;
-	
-	if (keyIsDown(Phaser.Keyboard.A)) {
-		slimy.body.velocity.x = -150;
-	} else if (keyIsDown(Phaser.Keyboard.D)) {
-		slimy.body.velocity.x = 150;
-	}
 
-	if (keyIsDown(Phaser.Keyboard.W) && ~~slimy.body.velocity.y == 0) {
-		slimy.body.velocity.y = -350;
-	}
-
-	if (slimy.input.isDragged) {
-		slimy.body.velocity.x = 0;
-		slimy.body.velocity.y = 0;
-	}
 }
--- js/util.js
+++ js/util.js
@@ -1,3 +1,10 @@
+// CONSTANTS
+var W_KEY = Phaser.Keyboard.W;
+var S_KEY = Phaser.Keyboard.S;
+var A_KEY = Phaser.Keyboard.A;
+var D_KEY = Phaser.Keyboard.D;
+var J_KEY = Phaser.Keyboard.J;
+
 // DEBUG
 function log(msg) {
 	console.log(msg);
@@ -6,12 +13,16 @@ function log(msg) {
 
 // BASIC GAME FUNCTIONS
 function createBasicGame(WIDTH, HEIGHT, type) {
-	var game = new Phaser.Game(WIDTH, HEIGHT, type, '', {preload:preload, create:create, update:update});
+	game = new Phaser.Game(WIDTH, HEIGHT, type, '', {preload:preload, create:create, update:update});
 	return game;
 }
 
-function createGroup(group) {
-	group = game.add.group();
+function addState(id, state, startOnCreate) {
+	game.state.add(id, state, startOnCreate);
+}
+
+function createGroup() {
+	return game.add.group();
 }
 
 function loadImage(id, path) {
@@ -26,8 +37,65 @@ function loadAudio(id, path) {
 	game.load.audio(id, path);
 }
 
+function loadTilemap(id, path, type) {
+	game.load.tilemap(id, path, null, type);
+}
+
+function cameraFollow(sprite) {
+	game.camera.follow(sprite);
+}
+
+function addImage(x, y, image) {
+	return game.add.image(x, y, image);
+}
+
+function setTimedEvent(delay, callback, callbackContext) {
+	game.time.events.loop(delay, callback, callbackContext);
+}
+
+function setBackgroundColor(color) {
+	game.stage.backgroundColor = color;
+}
+
+function addButton(x, y, id, actionOnClick, overFrame, outFrame, downFrame) {
+	return game.add.button(x, y, id, actionOnClick, this, overFrame, outFrame, downFrame);
+}
+
+function fadeIn(sprite, duration, ease, autoStart) {
+	sprite.alpha = 0;
+	game.add.tween(sprite).to({alpha:1}, duration, ease, autoStart);
+}
+
+function fadeOut(sprite, duration, ease, autoStart) {
+	sprite.alpha = 1;
+	game.add.tween(sprite).to({alpha:0}, duration, ease, autoStart);
+}
+
+function flash(sprite, duration, ease, autoStart) {
+	sprite.alpha = 1;
+	game.add.tween(sprite).to({alpha:0}, duration, ease, autoStart, 0, 1000, true);
+}
+
+function tileToCoord(x) {
+	return x * 32;
+}
+
+
+// NPCS
+function createNPC(npc, x, y, frame, msg, afterDialogFunction) {
+	var buffer = addSprite(x, y, 'npc');
+	buffer.dialog = msg;
+	setFrame(buffer, frame);
+	buffer.afterDialogFunction = afterDialogFunction;
+	npc.add(buffer)
+}
+
 
 // SPRITES
+function addSprite(x, y, id) {
+	return game.add.sprite(x, y, id);
+}
+
 function enableInput(sprite) {
 	sprite.inputEnabled = true;
 }
@@ -37,13 +105,49 @@ function disableInput(sprite) {
 }
 
 
+// TILEMAPS
+function addTilemap(id) {
+	map = game.add.tilemap(id);
+}
+
+function bindTiles(map, id, tiles) {
+	map.addTilesetImage(id, tiles);
+}
+
+function setCollision(rng1, rng2) {
+	map.setCollisionBetween(rng1, rng2);
+}
+
+function createLayer(name) {
+	layer = map.createLayer(name);
+}
+
+
+// RETROFONTS
+function addFont(id, width, height, textset, charPerRow) {
+	return game.add.retroFont(id, width, height, textset, charPerRow);
+}
+
+function setText(font, text) {
+	font.text = text;
+}
+
+
 // ARCADE PHYSICS
+function startArcadePhysics() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+}
+
 function enableArcadePhysics(sprite) {
 	game.physics.arcade.enable(sprite);
 }
 
 function checkCollision(group1, group2) {
 	game.physics.arcade.collide(group1, group2);
+}
+
+function checkCollision(group1, group2, functionToDo) {
+	game.physics.arcade.collide(group1, group2, functionToDo, null, this);
 }
 
 function checkOverlap(group1, group2, collideCallback, processCallback, callbackContext) {
@@ -55,10 +159,37 @@ function createArcadePhysicsGroup(group) {
 	group.enableBody = true;
 }
 
+function setVelocityX(sprite, x) {
+	sprite.body.velocity.x = x;
+}
+
+function setVelocityY(sprite, y) {
+	sprite.body.velocity.y = y;
+}
+
+function setVelocity(sprite, x, y) {
+	setVelocityX(sprite, x);
+	setVelocityY(sprite, y);
+}
+
+function setAccelerationX(sprite, x) {
+	sprite.body.acceleration.x = x;
+}
+
+function setAccelerationY(sprite, y) {
+	sprite.body.acceleration.y = y;
+}
+
+function setAcceleration(sprite, x, y) {
+	setAccelerationX(sprite, x);
+	setAccelerationY(sprite, y);
+}
+
+
 
 // AUDIO
-function addMusic(id, music) {
-	music = game.add.audio(id);
+function addMusic(id) {
+	return game.add.audio(id, 1, true);
 }
 
 function playMusic(music) {
